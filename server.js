24b6c8e40958a22ed9e3985319e9a61a18b38539
adrummer1 <andrew@bluestemcreativeco.com>
2023-09-11 10:43:49 -0500

    Updated server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+
+const sess = {
+    secret: 'keyboard cat',
+    cookie: {}
+}
+
+if (app.get('env') = 'production') {
+    app.set('trust proxy', 1)
+    sess.cookie.secure = true
+}
+
+app.use(session(sess))
+
+const hbs = exphbs.create({
+    helpers: {
+        format_date: date => {
+            return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        }
+    }
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public/')));
+
+app.use(require('./controllers'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
+
+
+
